b056f858134a16817b395ba234b584e845db3bec
Nik Rykov <rykov.nikas@gmail.com>
2023-02-23 23:50:49 +0100

    Fix processing of already processed transactions

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stateller - the decentralized state framework.
+
+Stateller is appchain framework designed for bootstrapping app-specific state machines from very zero, without relying on another chains for security.
+
+Stateller contains functionality for p2p and consensus, using FwPoA consensus mechanism for fair and secure system bootstrapping and existance.
+
+## Fixed-weight Proof-of-Authority (FwPoA)
+
--- tx-core/handler.js
+++ tx-core/handler.js
@@ -2,8 +2,11 @@ let crypto = require("crypto");
 let path = require("path")
 let fs = require('fs')
 module.exports = async (tx) => {
+    let txId = crypto.createHash("sha256").update(JSON.stringify({ input: tx.input, type: tx.type, anchoredTxId: tx.anchoredTxId, expires: tx.expires, signature: tx.signature })).digest("base64url")
+    if (state.processedTransactions.includes(txId)) { return { error: "Already processed" } }
     state.sequence++
-    state.lastTxIds[tx.signer] = crypto.createHash("sha256").update(JSON.stringify({ input: tx.input, type: tx.type, anchoredTxId: tx.anchoredTxId, expires: tx.expires, signature: tx.signature })).digest("base64url")
+    state.processedTransactions.push(txId)
+    state.lastTxIds[tx.signer] = txId
     if (fs.existsSync(path.join(__dirname, "transactions", tx.type + ".js"))) {
         await (require(path.join(__dirname, "transactions", tx.type + ".js")))(tx)
     }
