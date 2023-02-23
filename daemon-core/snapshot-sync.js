565907342a9beaa9633cfedaddadf3d28fe1f59e
Nik Rykov <rykov.nikas@gmail.com>
2023-02-23 20:16:02 +0100

    fix

--- daemon-core/snapshot-sync.js
+++ daemon-core/snapshot-sync.js
@@ -1,4 +1,5 @@
 let { fetch } = require("undici")
+let { bls12_381: bls } = require('@noble/curves/bls12-381');
 module.exports = async () => {
 
     for (peer of peers) {
@@ -13,19 +14,21 @@ async function checkForNewSnapshots(peer) {
         return console.error("Failed to fetch snapshots from " + peer)
     }
     for (snapshot of newSnapshots.snapshots) {
-        await attemptAddingPowerSnapshot(snapshot)
+        await attemptAddingPowerSnapshot(snapshot).catch(e => console.error(e))
     }
 
 }
 async function attemptAddingPowerSnapshot(snapshotString) {
+    console.log(snapshotString)
     let signature = Buffer.from(snapshotString.split(":")[0].split(";")[0], "base64")
-    let signers = snapshotString.split(":")[0].split(";")[1].split(",").map(signerPubkey => Buffer.from(signerPubkey, "base64"))
+    let signers = snapshotString.split(":")[0].split(";").map(signerPubkey => Buffer.from(signerPubkey, "base64"))
     let newPowerSet = snapshotString.split(":")[1].split(";").map(pubkey => Buffer.from(pubkey, "base64"))
     let correlatedSigners = currentPowerSet.filter(signerInActivePowerSet => signers.find(signer => signer.toString("base64") == signerInActivePowerSet.toString("base64")))
     if (Math.floor(correlatedSigners.length / 2) + 1 < currentPowerSet.length) {
         return console.warn("[Updating power set] Threshold for power update is not met")
     }
-    if (!bls.verify(signature, snapshotString.split(":")[1], signers)) {
+    console.log(signature, Buffer.from(snapshotString.split(":")[1], "base64"), signers)
+    if (!bls.verify(signature.toString("hex"), Buffer.from(snapshotString.split(":")[1], "base64").toString("hex"), bls.aggregatePublicKeys(signers))) {
         return console.warn("[Updating power set] Snapshot rejected due to failure of signature verification")
     }
     currentPowerSet = newPowerSet;
