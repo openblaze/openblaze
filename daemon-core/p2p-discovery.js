5e8e1c06087fb164a16b8aa38175b4a5cf08c717
Nik Rykov <rykov.nikas@gmail.com>
2023-02-24 13:58:42 +0100

    fix

--- daemon-core/p2p-discovery.js
+++ daemon-core/p2p-discovery.js
@@ -19,11 +19,11 @@ async function addPeersFrom(bootstrap) {
         console.log("Failed to fetch peers from " + bootstrap)
         return
     }
-    if (subpeers.filter(sp => !peers.has(sp)).length < 1) {
+    if (subpeers.filter(sp => peers.has(sp)).length < 1) {
         return
     }
     subpeers = subpeers.filter(subpeer => peerFailureCounter[subpeer] < 10)
-    if (subpeers.filter(sp => peers.has(sp).length < 1)) {
+    if (subpeers.filter(sp => peers.has(sp)).length < 1) {
         console.log("Added peers " + subpeers.filter(sp => peers.has(sp)).join(", "))
     }
     peers = new Set([...peers, ...subpeers])
