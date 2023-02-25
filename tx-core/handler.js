1c84fbfaaf78681b3156cace575deae63b30f07f
Nik Rykov <rykov.nikas@gmail.com>
2023-02-25 04:27:13 +0100

    fix throwing when tx throws

--- tx-core/handler.js
+++ tx-core/handler.js
@@ -3,12 +3,15 @@ let path = require("path")
 let fs = require('fs')
 module.exports = async (tx) => {
     let txId = crypto.createHash("sha256").update(JSON.stringify({ input: tx.input, type: tx.type, anchoredTxId: tx.anchoredTxId, expires: tx.expires, signature: tx.signature })).digest("base64url")
+    tx.id = txId
     if (state.processedTransactions.includes(txId)) { return { error: "Already processed" } }
     state.sequence++
     state.processedTransactions.push(txId)
     state.lastTxIds[tx.signer] = txId
     if (fs.existsSync(path.join(__dirname, "transactions", tx.type + ".js"))) {
-        await (require(path.join(__dirname, "transactions", tx.type + ".js")))(tx)
+        await (require(path.join(__dirname, "transactions", tx.type + ".js")))(tx).catch(error => {
+            console.error("[TX EXECUTION ERROR] (" + txId + ") ", error)
+        })
     }
     needToWrite.add("state")
     return { ok: true }
