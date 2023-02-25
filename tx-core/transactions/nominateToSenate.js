4b99c0b0fe10244170f5b0b2b39ca0f5d8f78f48
Nik Rykov <rykov.nikas@gmail.com>
2023-02-25 02:06:24 +0100

    all the test utils

--- tx-core/transactions/nominateToSenate.js
+++ tx-core/transactions/nominateToSenate.js
@@ -1,5 +1,6 @@
 module.exports = async (tx) => {
     if (!tx.input || !tx.input.name || !tx.input.description || !tx.input.pubkey || !tx.input.links || !tx.input.contacts) {
+        console.log("Not enough info")
         throw new Error("Not enough info")
     }
     state.senateCandidates[tx.input.pubkey] = { name: tx.input.name, description: tx.input.description, pubkey: tx.input.pubkey, links: tx.input.links, contacts: tx.input.contacts, votes: [] }
