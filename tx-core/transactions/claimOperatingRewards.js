581653bd982170d57ecfed7ea16871c78d3feb54
Nik Rykov <rykov.nikas@gmail.com>
2023-02-28 13:16:58 +0100

    estimated rewards query

--- tx-core/transactions/claimOperatingRewards.js
+++ tx-core/transactions/claimOperatingRewards.js
@@ -5,7 +5,7 @@ module.exports = async (tx) => {
     if (!currentPowerSet.find(senatorpk => senatorpk.toString("base64url") == tx.signer)) {
         throw new Error("Signer is not senator")
     }
-    if (Math.abs(tx.input.time - Math.round(Object.values(state.clock).reduce((pv, cv) => pv + cv, 0) / state.clock)) > 120000) {
+    if (Math.abs(tx.input.time - Math.round(Object.values(state.clock).reduce((pv, cv) => pv + cv, 0) / Object.values(state.clock).length)) > 120000) {
         throw new Error("Clock is out of sync")
     }
     let elapsedTime = tx.input.time - state.senators[tx.signer].lastRewardsClaim
@@ -13,7 +13,7 @@ module.exports = async (tx) => {
         throw new Error("Can claim operating rewards only once every 30 minutes")
     }
     let rewards = 100n * BigInt(elapsedTime / 100000) //100s is put specially to incentivize rare reward claim and disincentivize reward claim spam (if senator claims once per 30m, part of rewards gets lost due to rounding, the more time the less rounding impact)
-    state.balances[tx.signer].amount = (BigInt(state.balances[tx.signer].amount) + rewards).toString()
+    state.balances[tx.signer].amount = (BigInt(Math.floor(state.balances[tx.signer].amount) + rewards)).toString()
 
     state.senators[tx.signer].lastRewardsClaim = tx.input.time
 }
