f4db53defd5fb901eae4a5c5f7e31f50519fc35c
ducksquaddd <ducksquadddd@gmail.com>
2023-03-02 00:12:06 +0100

    adds denoms

--- tx-core/transactions/claimOperatingRewards.js
+++ tx-core/transactions/claimOperatingRewards.js
@@ -12,8 +12,9 @@ module.exports = async (tx) => {
     if (elapsedTime < 1.8e+6) {
         throw new Error("Can claim operating rewards only once every 30 minutes")
     }
+    
     let rewards = 100n * BigInt(Math.floor(elapsedTime / 100000)) //100s is put specially to incentivize rare reward claim and disincentivize reward claim spam (if senator claims once per 30m, part of rewards gets lost due to rounding, the more time the less rounding impact)
-    state.balances[tx.signer].amount = (BigInt(Math.floor(state.balances[tx.signer].amount)) + rewards).toString()
+    state.balances[tx.signer][state.params.denom].amount = (BigInt(Math.floor(state.balances[tx.signer][state.params.denom].amount)) + rewards).toString()
 
     state.senators[tx.signer].lastRewardsClaim = tx.input.time
 }
